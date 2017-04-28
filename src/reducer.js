f051f4d7374127181644bf0941b387a51905eba3
Walter Kjell Dahl <waldah@knowit.no>
2017-04-28 14:20:39 +0200

    Fikset setting av state

--- src/reducer.js
+++ src/reducer.js
@@ -3,17 +3,14 @@ import {Map, List} from 'immutable';
 const posts = 'posts';
 const feedback = 'feedback';
 const user = 'user';
-const init = Map({user: {}, posts: [{"id": 1, "type": "post"}], feedback: List});
+const init = Map({user: Map(), posts: [{"id": 1, "type": "post"}], feedback: List});
 
 export default function (state = init, action) {
     switch (action.type) {
         case 'ADD_FEEDBACK':
             return state;
         case 'UPDATE_LOGINFORM':
-            console.log('action.payload ', action.payload);
-            var newState = state.setIn(user, action.payload);
-            console.log('name' , newState.get('name'));
-            return newState;
+            return state.setIn([user, 'name'], action.payload.name);
         default:
             return state;
     }
