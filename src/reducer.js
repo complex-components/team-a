21b97941a5e2bb8e2c713e41796b0870cb69af4a
Walter Kjell Dahl <waldah@knowit.no>
2017-04-28 12:05:13 +0200

    Basic state oppsett

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,16 @@
-import {Map} from 'immutable';
+import {Map, List} from 'immutable';
 
-
-const init = Map({});
+const posts = 'posts';
+const feedback = 'feedback';
+const user = 'user';
+const init = Map({user: {}, posts: [{"id": 1, "type": "post"}], feedback: List});
 
 export default function (state = init, action) {
     switch (action.type) {
+        case 'ADD_FEEDBACK':
+            return state;
+        case 'LOGIN_USER':
+            return state.set(user, action.payload);
         default:
             return state;
     }
